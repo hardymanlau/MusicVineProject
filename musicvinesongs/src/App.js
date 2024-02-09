b9f303708aeb329b8f958ae606f20315895676cb
hardymanlau <hardymanlau@gmail.com>
2024-02-09 11:41:31 +0000

    Edited the main app file and app css file with the DisplaySongs component and relevant styling

---
+++ musicvinesongs/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import DisplaySongs from './components/DisplaySongs';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DisplaySongs />
+      </header>
+    </div>
+  );
+}
+
+export default App;
